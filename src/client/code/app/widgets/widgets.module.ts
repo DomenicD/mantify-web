c80b1731cb1d041835be165b93ded0b1d9ed870d
Domenic Donato <info@domenicdonato.com>
2015-05-17 16:36:05 -0700

    Deleted the hot towel application shell.

--- src/client/code/app/widgets/widgets.module.ts
+++
@@ -1,8 +0,0 @@
-/// <reference path="../../../../../typings/tsd.d.ts" />
-import {htImgPerson} from './ht-img-person.directive';
-import {htWidgetHeader} from './ht-widget-header.directive';
-
-export var widgetsModule = angular
-	.module('app.widgets', [])
-	.directive('htImgPerson', htImgPerson)
-	.directive('htWidgetHeader', htWidgetHeader);
