f78b4e203822a288726e7fe5687b7236c2b42748
Domenic Donato <info@domenicdonato.com>
2015-05-16 22:08:31 -0700

    Installed NewAngularRouter, added skeleton components folder structure, and updated project's gulp serve-dev to work after clean clone from github

--- src/client/code/app/app.module.ts
+++ src/client/code/app/app.module.ts
@@ -5,29 +5,43 @@ import { dashboardModule } from './dashboard/dashboard.module';
 import { layoutModule } from './layout/layout.module';
 import { widgetsModule } from './widgets/widgets.module';
 
+import { AppController } from './app';
+import { componentsModule } from './components/components.module';
+
 let thirdPartyModules = [
-    'ngMaterial'
+    'ngMaterial',
+    'ngNewRouter'
 ];
 
 let internalModules = [
-    adminModule.name,
-    coreModule.name,
-    dashboardModule.name,
-    layoutModule.name,
-    widgetsModule.name
+    componentsModule.name
 ];
 
-angular.module('app', [].concat(
-    thirdPartyModules,
-    internalModules
-    ))
-    .config(ngMaterialConfig);
+let dependencyModules = thirdPartyModules.concat(internalModules);
 
-angular.bootstrap(document, ['app']);
+angular.module('app', dependencyModules)
+    .controller('AppController', AppController)
+    .config(ngMaterialConfig)
+    .config(newRouterConfig);
 
+angular.bootstrap(document.body, ['app']);
 
 function ngMaterialConfig($mdThemingProvider: angular.material.MDThemingProvider) {
     $mdThemingProvider.theme('default')
         .primaryPalette('green')
         .accentPalette('blue');    
+}
+
+function newRouterConfig($componentLoaderProvider) {
+    $componentLoaderProvider
+        .setTemplateMapping(name => {            
+            let dashName = dashCase(name); 
+            return `./src/client/code/app/components/${dashName}/${dashName}.html`;
+        });
+}
+
+function dashCase(str) {
+  return str.replace(/([A-Z])/g, function ($1) {
+    return '-' + $1.toLowerCase();
+  });
 }
